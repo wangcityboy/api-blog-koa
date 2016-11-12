4aaae0d01acb1633e51e4a2952ff018e09e41d35
云飞凌风 <yunfeiyang@macbook.local>
2016-11-12 21:37:41 +0800

    update

--- config/development.js
+++ config/development.js
@@ -14,24 +14,19 @@ module.exports = {
    */
   mysqlServers: [
     {
-      //host: (process.env.CS_MYSQL || 'hdm14969059.my3w.com'),
-      //port: 3306,
-      //user: 'hdm14969059',
-      //password: 'whf324512',
-      host: (process.env.CS_MYSQL || '127.0.0.1'),
+      host: (process.env.CS_MYSQL || '120.25.83.199'),
       port: 3306,
       user: 'root',
       password: 'KeYpZrZx'
     }
   ],
-  //mysqlDatabase: 'hdm14969059_db',
   mysqlDatabase:'guest',
   mysqlMaxConnections: 20,
   mysqlQueryTimeout: 5000,
 
   // mongodb 设置
   mongo: {
-    uri: 'mongodb://127.0.0.1:27017/yunfeiapp',
+    uri: 'mongodb://120.25.83.199:27017/yunfeiapp',
     options: {
       db: {native_parser: true},
       auto_reconnect: 1,
@@ -44,7 +39,7 @@ module.exports = {
   // redis config
   // use for koa-limit module as storage
   redis: {
-    host: '127.0.0.1',
+    host: '120.25.83.199',
     port: 6379,
     options: {
       //auth_pass: 'd53aOe1F9q'
