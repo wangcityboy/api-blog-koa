4ab0103a636b2928d7ec98cca76232f4e555db4e
王海峰 <wangcityboy@163.com>
2016-12-13 14:44:31 +0800

    update

--- model/notes.js
+++ model/notes.js
@@ -12,7 +12,7 @@ var mysql = require('../common/mysql.js');
  @values 条件值   格式为[value1，value2]
  */
 exports.getNotes = function*(fields, where, values) {
-    var data = yield mysql.query("select " + fields + " from tg_article where " + where, values);
+    var data = yield mysql.query("select " + fields + " from tg_article in " + where, values);
     if (data.length > 0) {
         return data;
     }
